783bd088e17d276db6a2bce6031562e42f49cb4d
Tanya Nobal <nobaltanya@gmail.com>
2023-05-23 16:06:01 -0700

    Added quantity, updated cart, validation functions

--- homework-1/shopping_cart.py
+++ homework-1/shopping_cart.py
@@ -14,6 +14,12 @@
 import uuid
 import re
 
+def validate_customer_id(customer_id):
+    valid_id = re.match(r'[a-zA-Z]{3}\d{5}[a-zA-Z]{2}[-][A|Q]', customer_id)
+    if not valid_id:
+        raise Exception("Not a valid customer id")
+    return customer_id
+
 class Item():
     #Creates an item object to be placed in the shopping cart.
     def __init__(self, name, price):
@@ -24,15 +30,15 @@ class Item():
 
     @property
     def item_id(self):
-        return self.item_id
+        return self.item_id.deepcopy()
 
     @property
     def item_name(self):
-        return self.item_name
+        return self.item_name.deepcopy()
 
     @property
     def price(self):
-        return self.price
+        return self.price.deepcopy()
 
     @price.setter
     def price(self, price):
@@ -40,40 +46,79 @@ class Item():
             raise ValueError("Price cannot be negative.")
         self.price = price
 
+class SKU(): 
+    def validated(code): 
+        # if code isn't a string, throw an error
+        if type(code) != "str":
+            raise TypeError("SKU must be a string.")
+        # if code doesn't match the regex, throw an error
+        if (re.match(r'[A-Z]{3}_[A-Z]{3}_\d{2}$', code)):
+            raise TypeError("SKU improperly formatted.")
+            
+        return code
+    def __init__(self, code):
+        code = validated(code)
+        self.code = code
+
+    
+
+
+class Quantity():
+    def validate(quantity):
+        if type(quantity) != int:
+            raise TypeError("Quantity must be an integer")
+        if quantity <= 0:
+            raise Error("Quantity must be greater than 0")
+        if quantity > 1000:
+            raise Error("Quantity can't be greater than 1000")
+        return quantity
+    
+    def __init__(self, quantity):
+        self.quantity = self.validate(quantity)
+
+    @property
+    def quantity(self):
+        return self.quantity.deepcopy()
+
 class ShoppingCart(object):
     #Creates shopping cart objects for users of our fine website.
-    
-    items_in_cart = {}
 
-    def __init__(self, owner_of_cart):
-        self.owner_of_cart = owner_of_cart
+    def __init__(self, customer_id):
         self.cart_id = uuid.uuid4()
-        self.customer_id = re.compile(r'[a-zA-Z]{3}\d{5}[a-zA-Z]{2}[-][A|Q]')
+        self.customer_id = validate_customer_id(customer_id)
+        self.items_in_cart = {}
 
     @property
     def cart_id(self):
-        return self.cart_id
+        return self.cart_id.deepcopy()
     
     @property
     def customer_id(self):
-        return self.customer_id
+        return self.customer_id.deepcopy()
+    
+    @property
+    def items_in_cart(self):
+        return self.items_in_cart.deepcopy()
 
     def add_item(self, product, quantity):
-        # Add product to the cart.
         # validate SKU
         # validate product is in catalog
-        # validate quantity is greater than 0
-
-        if not product in self.items_in_cart:
-            self.items_in_cart[product] = price
-            print (product + " added.")
-        else:
-            print (product + " is already in the cart.")
-
-    def remove_item(self, product):
-        # Remove product from the cart.
+        Quantity.validate(quantity)
         if product in self.items_in_cart:
-            del self.items_in_cart[product]
-            print (product + " removed.")
+            self.items_in_cart[product] += quantity
+        else:
+            self.items_in_cart[product] = quantity
+        
+
+    def remove_item(self, product, quantity):
+        # Remove product from the cart.
+        # validate SKU
+        if product in self.items_in_cart:
+            if quantity > self.items_in_cart[product]:
+                print ("Quantity requested exceeds quantity in cart.")
+            else:
+                self.items_in_cart[product] -= quantity
+                print (quantity + " of " + product + " removed.")
         else:
             print (product + " is not in the cart.")
+            
