c2de7f9367d828beaebff7c157e7f40e7943a38b
Jason Douglas <jasoncd31@gmail.com>
2023-06-09 14:01:04 -0700

    updated c stack class

--- homework-3/c/Stack.c
+++ homework-3/c/Stack.c
@@ -8,53 +8,77 @@
 
 #define STACK_UNDERFLOW -1
 #define STACK_OVERFLOW -2
+#define NULL_POINTER -3
+#define MAX_CAPACITY 2048 
+
+struct stack {
+    char** elements;
+    unsigned int top;
+    unsigned int capacity;
+};
 
 Stack *stack_create(unsigned int capacity) {
-    Stack *stack = (Stack *) malloc(sizeof(Stack));
+    Stack stack = (Stack) malloc(sizeof(Stack));
     stack->capacity = capacity;
     stack->top = -1;
-    stack->array = (char**) malloc(stack->capacity * sizeof(char*));
+    stack->elements = (char**) malloc(stack->capacity * sizeof(char*));
     return stack;
 }
 
-bool stack_is_empty(Stack *stack) {
+bool stack_is_empty(const Stack stack) {
     return stack->top == -1;
 }
 
-bool stack_is_full(Stack *stack) {
+bool stack_is_full(Stack stack) {
     return stack->top == stack->capacity - 1;
 }
 
-void stack_push(Stack *stack, char* item) {
-    if (stack_is_full(stack)) {
+void stack_push(Stack stack, char* item) {
+    if (strlen(item) > MAX_CAPACITY) {
         exit(STACK_OVERFLOW);
     }
-    stack->array[++stack->top] = item;
+    if (item == NULL) {
+        exit(NULL_POINTER);
+    }
+    if (stack_is_full(stack)) {
+        stack->elements = realloc(stack->elements, stack->capacity * 2);
+        stack->capacity *= 2;
+    }
+    stack->elements[++stack->top] = item;
 }
 
-char* stack_pop(Stack *stack) {
+char* stack_pop(Stack stack) {
+    //you can null out the memory
     if (stack_is_empty(stack)) {
         exit(STACK_UNDERFLOW);
     }
-    return stack->array[stack->top--];
+    else if (stack->top < stack->capacity / 2) {
+        stack->elements = realloc(stack->elements, stack->capacity / 2);
+        stack->capacity /= 2;
+    }
+    char* item = stack->elements[stack->top--];
+    stack->elements[stack->top + 1] = NULL;
+    return item;
 }
 
-char* stack_peek(Stack *stack) {
+char* stack_peek(Stack stack) {
     if (stack_is_empty(stack)) {
         exit(STACK_UNDERFLOW);
     }
-    return stack->array[stack->top];
+    return stack->elements[stack->top];
 }
 
-void stack_destroy(Stack *stack) {
-    free(stack->array);
+void stack_destroy(Stack stack) {
+    free(stack->elements);
     free(stack);
 }
 
 int main() {
-    Stack *stack = stack_create(10);
+    Stack *stack = stack_create(2);
     stack_push(stack, "Hello");
     stack_push(stack, "World");
+    stack_push(stack, "!");
+    stack_push(stack, "?");
     printf("%s\n", stack_pop(stack));
     printf("%s\n", stack_pop(stack));
     stack_destroy(stack);
--- homework-3/c/stack.h
+++ homework-3/c/stack.h
@@ -5,20 +5,15 @@
 
 #include <stdbool.h>
 
-typedef struct stack Stack;
-struct stack {
-    char** array;
-    int top;
-    int capacity;
-};
+typedef struct stack* Stack;
 
 Stack *stack_create(unsigned int capacity);
-void stack_destroy(Stack *s);
-void stack_push(Stack *s, char* value);
-char *stack_pop(Stack *s);
-char *stack_peek(Stack *s);
-bool stack_is_empty(Stack *s);
-bool stack_is_full(Stack *s);
-int stack_getSize(Stack *s);
+void stack_destroy(Stack s);
+void stack_push(Stack s, char* value);
+char *stack_pop(Stack s);
+char *stack_peek(Stack s);
+bool stack_is_empty(Stack s);
+bool stack_is_full(Stack s);
+int stack_getSize(Stack s);
 
 #endif
