6c6da6d363c9102f48bfaf0998c3455eedde022a
Jason Douglas <jasoncd31@gmail.com>
2023-06-22 15:37:13 -0700

    working node commands for problem 4

--- homework-4/cipherJS/index.js
+++ homework-4/cipherJS/index.js
@@ -1,9 +1,38 @@
 import { createCipheriv, createDecipheriv } from 'crypto'
 
-const cipher = (createCipheriv(
-    'aes-256-cbc',
-    "thisisa_32_byte_long_key_I_think",
-    "dog1234567890123"
-))
+const args = process.argv.slice(2)
 
-console.log(cipher.update("ow are you today?", "utf8", "hex") + cipher.final("hex"))
+if (args.length != 4) {
+    console.log("Please enter commands in the following structure: node index.js -e/-d string key iv")
+    process.exit(1)
+}
+
+if (args[0] === "-e") {
+    console.log("encrypting")
+    console.log(encrypt(args[1], args[2], args[3]))
+} else if (args[0] === "-d") {
+    console.log("decrypting")
+    console.log(decrypt(args[1], args[2], args[3]))
+} else {
+    console.log("Please provide a valid flag (-e or -d)")
+    process.exit(1)
+}
+
+
+function encrypt(string, key, iv) {
+    const cipher = (createCipheriv(
+        'aes-256-cbc',
+        key,
+        iv
+    ))
+    return cipher.update(string, "utf8", "hex") + cipher.final("hex")
+}
+
+function decrypt(string, key, iv) {
+    const decipher = createDecipheriv(
+        'aes-256-cbc',
+        key,
+        iv
+    )
+    return decipher.update(string, "hex", "utf8") + decipher.final("utf8")
+}
